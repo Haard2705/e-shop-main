206688041d1c6f53fad604da1be426a606344546
Haard2705 <haard.pathak@gmail.com>
2025-06-02 15:10:23 -0400

    first commit

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import Container from "../components/container";
 import FormWrap from "../components/FormWrap";
