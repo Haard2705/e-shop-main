4227d58b1d0103cebeb6a2346d55d2c74c870c6f
Haard2705 <haard.pathak@gmail.com>
2025-06-02 16:24:55 -0400

    first commit

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import getCurrentUser from "@/actions/getCurrentUser";
 import Container from "../components/container";
 import CartClient from "./CartClient";
